e53cccffae68482bdc6f463f9d1a3c3126ec42f2
brenosavio <brenosavio@gmail.com>
2023-03-04 17:31:59 -0400

    Aula 35 - Loraine - crud Angular x Spring

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,23 +1,52 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Course} from "../model/course";
-import {delay, first, take, tap} from "rxjs/operators";
+import {HttpClient} from '@angular/common/http';
+
+import {Course} from '../model/course';
+import {first} from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class CoursesService {
 
-  private readonly API = '/assets/courses.json';
+  private readonly API = 'api/courses';
 
-  // @ts-ignore
-  constructor(private httpCliente: HttpClient) { }
 
-  list(){
-    return this.httpCliente.get<Course[]>(this.API)
+  constructor(private httpClient: HttpClient) {
+  }
+
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
       .pipe(
         first(),
-        tap(courses => console.log(courses))
+        //delay(5000),
+        // tap(courses => console.log(courses))
       );
   }
+
+  loadById(id: string){
+    return this.httpClient.get<Course>(`${this.API}/${id}`);
+  }
+
+  save(record: Partial<Course>) {
+    if(record._id){
+      return this.update(record);
+    }else
+      return this.create(record);
+  }
+
+  private create(record: Partial<Course>){
+    return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
+
+  private update(record: Partial<Course>){
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record).pipe(first());
+  }
+
+  remove(id: string){
+    return this.httpClient.delete(`${this.API}/${id}`).pipe(first());
+  }
+
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,26 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
-import {AppMaterialModule} from "./app-material/app-material.module";
-import { CategoryPipe } from './pipes/category.pipe';
+import { NgModule } from '@angular/core';
+import { ConfirmationDialogComponent } from '../courses/components/confirmation-dialog/confirmation-dialog.component';
 
+import { AppMaterialModule } from './app-material/app-material.module';
+import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
+import { CategoryPipe } from './pipes/category.pipe';
 
 
 @NgModule({
   declarations: [
     ErrorDialogComponent,
     CategoryPipe,
+    ConfirmationDialogComponent
 
   ],
   imports: [
     CommonModule,
     AppMaterialModule
-  ],
+    ],
   exports: [
     ErrorDialogComponent,
+    ConfirmationDialogComponent,
     CategoryPipe
   ]
 })
