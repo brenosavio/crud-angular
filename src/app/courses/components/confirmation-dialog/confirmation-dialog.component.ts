d37400fd3c030ef52889ce1f9dc3fb1b6364847b
brenosavio <brenosavio@gmail.com>
2023-03-05 10:22:51 -0400

    Aula 35 - Loraine - crud Angular x Spring

--- src/app/courses/components/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/courses/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,8 +1,5 @@
-
-
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-
 
 @Component({
   selector: 'app-confirmation-dialog',
@@ -23,3 +20,4 @@ export class ConfirmationDialogComponent implements OnInit {
     this.dialogRef.close(result);
   }
 }
+    
